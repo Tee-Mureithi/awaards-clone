f1d74ca7b1d230c84a884518882ba16046ca915b
Tee-Mureithi <esther.mureithi@student.moringaschool.com>
2022-04-09 20:52:28 +0300

    adds urls

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,7 @@ app_name = "main"
 
 
 urlpatterns = [
-    path("", views.homepage, name="homepage"),
+    # path("", views.homepage, name="homepage"),
     
     path("register", views.register_request, name="register")
 ]
