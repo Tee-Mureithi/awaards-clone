2f538096c2d8326325f3f798d84c35fd26b6513f
Tee-Mureithi <esther.mureithi@student.moringaschool.com>
2022-04-09 21:49:00 +0300

    adds models project

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 # from django.contrib.auth.decorators import login_required
 # Create your models here.
 from cloudinary.models import CloudinaryField
@@ -6,4 +7,32 @@ from cloudinary.models import CloudinaryField
 class Project(models.Model):
     title = models.Charfield(max_length=50)
     image = CloudinaryField('images')
-    description
+    description = models.TextField()
+    link = models.URLField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.title
+
+    def save_project(self):
+        self.save()
+
+    def delete_project(self):
+        self.delete()
+
+    def get_absolute_url(self):
+        return reverse('home')
+
+    @classmethod
+    def search_by_title(cls,search_term):
+        return cls.objects.filter(title__icontains = search_term)
+
+    @classmethod
+    def update_project(cls, id,title):
+        return cls.objects.filter(id = id).update(title=title)
+
+
+    class Meta:
+        ordering = ['-created_at']
