9bf69bb84fd4bb39351d2f0842d3128344b31608
Tee-Mureithi <esther.mureithi@student.moringaschool.com>
2022-04-09 22:16:57 +0300

    adds the signals.py app

--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 # from django.contrib.auth.decorators import login_required
 # Create your models here.
 from cloudinary.models import CloudinaryField
@@ -36,3 +37,40 @@ class Project(models.Model):
 
     class Meta:
         ordering = ['-created_at']
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(default="Hi I'm new here")
+    project = models.ForeignKey(Project,on_delete=models.DO_NOTHING,null=True,blank=True)
+    profile_photo = CloudinaryField('images',blank=True,null=True)
+    gender = models.CharField(max_length=20)
+    contact = models.CharField(max_length = 10,blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def projects_posts(self):
+        return self.project_set.all()
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    def __str__(self):
+        return str(self.user.username)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def get_absolute_url(self):
+        return reverse('profile',args=(str(self.id)))
+
+    @classmethod
+    def search_by_user(cls,search_term):
+        return cls.objects.filter(user__username__icontains = search_term)
+
+    @classmethod
+    def update_profile(cls, id,bio):
+        return cls.objects.filter(id = id).update(bio=bio)
