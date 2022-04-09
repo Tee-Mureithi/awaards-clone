4cd37965240debc1e6713814fafc5a592e0b96ed
Tee-Mureithi <esther.mureithi@student.moringaschool.com>
2022-04-09 21:45:08 +0300

    adds cloudinary

--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-from django.contrib.auth.decorators import login_required
+# from django.contrib.auth.decorators import login_required
 # Create your models here.
+from cloudinary.models import CloudinaryField
+
+class Project(models.Model):
+    title = models.Charfield(max_length=50)
+    image = CloudinaryField('images')
+    description
