ec5321257cb37e15d276274c7c0943229fa0005f
Tee-Mureithi <esther.mureithi@student.moringaschool.com>
2022-04-11 22:35:47 +0300

    heroku files

--- awaards/settings.py
+++ awaards/settings.py
@@ -16,6 +16,7 @@ import os
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
+import django_on_heroku
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -190,3 +191,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+django_on_heroku.settings(locals())
