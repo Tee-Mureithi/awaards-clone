98e26fac3af5d68e1f1bd268b83333cc08622049
Tee-Mureithi <esther.mureithi@student.moringaschool.com>
2022-04-12 14:48:40 +0300

    addds heroku

--- awaards/settings.py
+++ awaards/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 import os
 import dj_database_url
 import django_on_heroku
-from decouple import config,Csv
+# from decouple import config,Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
